5461f12f9f8ac3c7c275b46845e023130adcbf5f
Neto test <natalia.d.titova@gmail.com>
2024-12-23 00:59:07 +0300

    Read XLS and return objects

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,100 @@
 package org.example;
 
+import java.io.IOException;
+import java.sql.SQLOutput;
+import java.util.List;
+import java.util.Scanner;
+
+import static org.example.files.XlsFileToClassObject.*;
+
 public class Main {
-    public static void main(String[] args) {
-        {
-            //Для Влада
-        }
+    public static void main(String[] args) throws IOException {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Добро пожаловать в программу Aston");
+        //System.out.println("Для выхода из программы введите end");
+
+        System.out.println("Выберите класс: ");
+        System.out.println("1. Автомобиль");
+        System.out.println("2. Книга");
+        System.out.println("3. Корнеплод");
+
+        String input = scanner.nextLine();
+
+        int operation = Integer.parseInt(input);
+        String selectedClass = classSelected(operation);
+        System.out.println("Вы выбрали класс " + selectedClass);
+
+        System.out.println(" =========================");
+        System.out.println("Выберите метод заполнения:");
+        System.out.println("1. Ручной");
+        System.out.println("2. Рандомный");
+        System.out.println("3. Из файла");
+
+        input = scanner.nextLine();
+        int method = Integer.parseInt(input);
+
+        String methodSelected = methodSelected(method, selectedClass);
+        //System.out.println("Вы выбрали метод " + methodSelected);
+
     }
+
+    private static String classSelected(int operation) {
+        String selectedClass = "";
+        switch (operation) {
+            case 1: // автомобиль
+                selectedClass = "Car";
+                break;
+            case 2: // Книга
+                selectedClass = "Book";
+                break;
+            case 3: // корнеплод
+                selectedClass = "RootVegetable";
+                break;
+            default:
+                System.out.println("Такой операции нет");
+        }
+
+        return selectedClass;
+    }
+
+    private static String methodSelected(int operation, String classSelected) throws IOException {
+        String selectedMethod = "";
+        switch (operation) {
+            case 1: // ручной
+                selectedMethod = "input";
+                break;
+            case 2: // рандомный
+                selectedMethod = "random";
+                break;
+            case 3: // из файла
+                selectedMethod = "fromFile";
+                System.out.println("введите путь к файлу");
+                Scanner scanner = new Scanner(System.in);
+                // TODO: сделать через Проводник выбор документа
+                String FILELOCATION = scanner.nextLine();;
+
+                if (classSelected.equals("Car")) {
+                    List<Car> cars  = excelDataToListOfObjets_Car(FILELOCATION);
+                    System.out.println(cars);
+                }
+                else if (classSelected.equals("Book")) {
+                    List<Book> books = excelDataToListOfObjets_Book(FILELOCATION);
+                    System.out.println(books);
+                }
+                else if (classSelected.equals("RootVegetable")) {
+                    List<RootVegetable> rootVegetables = excelDataToListOfObjets_RootVegitables(FILELOCATION);
+                    System.out.println(rootVegetables);
+                }
+
+                break;
+            default:
+                System.out.println("Такой операции нет");
+        }
+
+        return selectedMethod;
+    }
+
+
 }
--- src/main/java/org/example/files/XlsFileToClassObject.java
+++ src/main/java/org/example/files/XlsFileToClassObject.java
@@ -1,2 +1,88 @@
-package org.example.files;public class XlsFileToClassObject {
+package org.example.files;
+
+import org.apache.poi.ss.usermodel.DataFormatter;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.example.Book;
+import org.example.Car;
+import org.example.RootVegetable;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class XlsFileToClassObject {
+
+    public static List<Car> excelDataToListOfObjets_Car(String fileLocation)
+            throws IOException {
+        FileInputStream file = new FileInputStream(new File(fileLocation));
+        XSSFWorkbook workbook = new XSSFWorkbook(file);
+        XSSFSheet sheet = workbook.getSheetAt(0);
+        List<Car> carData = new ArrayList<Car>();
+        DataFormatter dataFormatter = new DataFormatter();
+        for (int n = 1; n < sheet.getPhysicalNumberOfRows(); n++) {
+            Row row = sheet.getRow(n);
+
+            int i = row.getFirstCellNum();
+
+            Car car = new Car.Builder()
+                            .setPower(Integer.parseInt(dataFormatter.formatCellValue(row.getCell(i))))
+                            .setModel(dataFormatter.formatCellValue(row.getCell(++i)))
+                            .setYear(Integer.parseInt(dataFormatter.formatCellValue(row.getCell(++i))))
+                            .build();
+
+            carData.add(car);
+        }
+        return carData;
+    }
+
+    public static List<Book> excelDataToListOfObjets_Book(String fileLocation)
+            throws IOException {
+        FileInputStream file = new FileInputStream(new File(fileLocation));
+        XSSFWorkbook workbook = new XSSFWorkbook(file);
+        XSSFSheet sheet = workbook.getSheetAt(0);
+        List<Book> bookData = new ArrayList<Book>();
+        DataFormatter dataFormatter = new DataFormatter();
+        for (int n = 1; n < sheet.getPhysicalNumberOfRows(); n++) {
+            Row row = sheet.getRow(n);
+
+            int i = row.getFirstCellNum();
+
+            Book book = new Book.Builder()
+                    .setAuthor(dataFormatter.formatCellValue(row.getCell(i)))
+                    .setTitle(dataFormatter.formatCellValue(row.getCell(++i)))
+                    .setPages(Integer.parseInt(dataFormatter.formatCellValue(row.getCell(++i))))
+                    .build();
+
+            bookData.add(book);
+        }
+        return bookData;
+    }
+
+    public static List<RootVegetable> excelDataToListOfObjets_RootVegitables(String fileLocation)
+            throws IOException {
+        FileInputStream file = new FileInputStream(new File(fileLocation));
+        XSSFWorkbook workbook = new XSSFWorkbook(file);
+        XSSFSheet sheet = workbook.getSheetAt(0);
+        List<RootVegetable> rootVegitablesData = new ArrayList<RootVegetable>();
+        DataFormatter dataFormatter = new DataFormatter();
+        for (int n = 1; n < sheet.getPhysicalNumberOfRows(); n++) {
+            Row row = sheet.getRow(n);
+
+            int i = row.getFirstCellNum();
+
+            RootVegetable veggi = new RootVegetable.Builder()
+                    .setType(dataFormatter.formatCellValue(row.getCell(i)))
+                    .setWeight(Integer.parseInt(dataFormatter.formatCellValue(row.getCell(++i))))
+                    .setColor(dataFormatter.formatCellValue(row.getCell(++i)))
+                    .build();
+
+            rootVegitablesData.add(veggi);
+        }
+        return rootVegitablesData;
+    }
+
 }
